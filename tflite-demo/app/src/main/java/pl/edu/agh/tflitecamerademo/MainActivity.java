24b2927744dac3fa1977ce9d443fa66fe3a92dd5
Michał Wolny <m.wolny@avsystem.com>
2018-06-12 22:26:49 +0200

    hook trained bayes to main activity

--- tflite-demo/app/src/main/java/pl/edu/agh/tflitecamerademo/MainActivity.java
+++ tflite-demo/app/src/main/java/pl/edu/agh/tflitecamerademo/MainActivity.java
@@ -2,13 +2,18 @@ package pl.edu.agh.tflitecamerademo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.io.IOException;
 
+import de.daslaboratorium.machinelearning.classifier.Classification;
+import pl.edu.agh.tflitecamerademo.bayes.TrainedBayes;
+
+public class MainActivity extends Activity {
 
     Button evaluateButton;
     Button trainButton;
@@ -17,6 +22,7 @@ public class MainActivity extends Activity {
     Switch positiveSwitch;
     EditText sentenceEditText;
 
+    TrainedBayes trainedBayes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,25 @@ public class MainActivity extends Activity {
         bayesResultTextView = findViewById(R.id.bayesResultTextView);
         positiveSwitch = findViewById(R.id.positiveSwitch);
         sentenceEditText = findViewById(R.id.sentenceEditText);
-    }
 
+        try {
+            trainedBayes = new TrainedBayes(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        evaluateButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Classification<String, String> classify = trainedBayes.classify(sentenceEditText.getText().toString());
+                        bayesResultTextView.setText(
+                                String.format("%s %s", classify.getCategory(), classify.getProbability())
+                        );
+                    }
+                }
+        );
+    }
 }
--- tflite-demo/app/src/main/java/pl/edu/agh/tflitecamerademo/bayes/TrainedBayes.java
+++ tflite-demo/app/src/main/java/pl/edu/agh/tflitecamerademo/bayes/TrainedBayes.java
@@ -5,9 +5,9 @@ import android.os.Environment;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -20,22 +20,24 @@ import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
 public final class TrainedBayes {
 
     private static final String DATASET_PATH = "bayes_train.csv";
-    private static final String MODEL_FILE = "bayes.model";
 
-    private final BayesClassifier<String, String> classifier;
+    private BayesClassifier<String, String> classifier = new BayesClassifier<>();
 
     private static final File modelFile = new File(
             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-            MODEL_FILE
+            "bayes.model"
     );
 
     private void pretrain(Context context) throws IOException {
+        classifier.setMemoryCapacity(10000);
         BufferedReader reader = new BufferedReader(new InputStreamReader(context.getAssets().open(DATASET_PATH)));
 
         String line;
-        while ((line = reader.readLine()) != null) {
+        int c = 0;
+        while ((line = reader.readLine()) != null && c < 10000) {
             String label = String.valueOf(line.charAt(0));
             learn(label, line.substring(2));
+            c++;
         }
         reader.close();
 
@@ -47,7 +49,7 @@ public final class TrainedBayes {
     }
 
     private static BayesClassifier<String, String> pretrained(Context context) throws IOException, ClassNotFoundException {
-        InputStream fis = context.getAssets().open(DATASET_PATH);
+        FileInputStream fis = new FileInputStream(modelFile);
         ObjectInputStream is = new ObjectInputStream(fis);
         BayesClassifier<String, String> result = (BayesClassifier<String, String>) is.readObject();
         is.close();
@@ -56,7 +58,7 @@ public final class TrainedBayes {
     }
 
     public TrainedBayes(Context context) throws IOException, ClassNotFoundException {
-        //pretrain(context);
+        pretrain(context);
         classifier = pretrained(context);
     }
 
